d8741002d6eb37d3623624ce85c666945ad57406
Marcelo Adrian Cordero <marceloadriancordero@gmail.com>
2021-03-20 18:01:25 +0100

    Creación del proyecto

---
+++ Merge-it/src/main/java/com/twodevs/Merge/it/MergeItApplication.java
@@ -0,0 +1,13 @@
+package com.twodevs.Merge.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergeItApplication.class, args);
+	}
+
+}
---
+++ Merge-it/src/test/java/com/twodevs/Merge/it/MergeItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twodevs.Merge.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
