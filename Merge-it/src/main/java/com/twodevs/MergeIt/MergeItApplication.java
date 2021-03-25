73e3f97a3eebaad8ceb3921ff7b8a4f8bc76ec7b
Marcelo Adrian Cordero <marceloadriancordero@gmail.com>
2021-03-25 21:38:44 +0100

    Estructura de proyecto

--- Merge-it/src/main/java/com/twodevs/MergeIt/MergeItApplication.java
+++ Merge-it/src/main/java/com/twodevs/MergeIt/MergeItApplication.java
@@ -1,4 +1,4 @@
-package com.twodevs.Merge.it;
+package com.twodevs.MergeIt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
